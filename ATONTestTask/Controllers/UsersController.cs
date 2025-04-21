4b7270b138052c98bc867f0296b6ef4e09207351
magomed <magomedyasulov8@gmail.com>
2025-04-22 01:47:19 +0300

    add: валидация полей при логине

--- ATONTestTask/Controllers/UsersController.cs
+++ ATONTestTask/Controllers/UsersController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ATONTestTask.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class UsersController : BaseController
     {
---
+++ ATONTestTask/Validators/LoginValidator.cs
@@ -0,0 +1,14 @@
+﻿using ATONTestTask.ViewModels.Request;
+using FluentValidation;
+
+namespace ATONTestTask.Validators
+{
+    public class LoginValidator : AbstractValidator<LoginDto>
+    {
+        public LoginValidator()
+        {
+            RuleFor(l => l.Login).NotEmpty();
+            RuleFor(l => l.Password).NotEmpty();
+        }
+    }
+}
